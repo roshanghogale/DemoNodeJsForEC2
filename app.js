3b32f1659c37a35e62cc3aedfee3a4bd00bf4d41
Roshan Ghogale <inglevaibhav667.com>
2025-11-28 15:34:06 +0530

    Initial commit: Roshan's Node.js EC2 AutoScaling Demo

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = 3000;
+
+// Set view engine and views directory
+app.set('view engine', 'html');
+app.set('views', path.join(__dirname, 'views'));
+
+// Middleware to serve static files
+app.use(express.static('public'));
+
+// Routes
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'home.html'));
+});
+
+app.get('/about', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'about.html'));
+});
+
+app.get('/guide', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'guide.html'));
+});
+
+app.get('/autoscaling', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'step5.html'));
+});
+
+// Start server
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
